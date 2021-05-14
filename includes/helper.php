f32dc6bf7f6467cc41b6f59dd6e935e8ffbfda6e
jlmobbex <javier.lopez@mobbex.com>
2021-05-14 19:41:41 -0300

    Add helper and utils

--- includes/helper.php
+++ includes/helper.php
@@ -1,14 +1,13 @@
 <?php
 require_once 'utils.php';
 
-class MobbexSubsHelper
+class Mbbx_Subs_Helper
 {
-
     public function __construct()
     {
         // Init settings (Full List in WC_Gateway_Mobbex_Subs::init_form_fields)
         $option_key = 'woocommerce_' . MOBBEX_SUBS_WC_GATEWAY_ID . '_settings';
-        $settings = get_option($option_key, null);
+        $settings = get_option($option_key, null) ?: [];
         foreach ($settings as $key => $value) {
             $key = str_replace('-', '_', $key);
             $this->$key = $value;
@@ -78,4 +77,151 @@ class MobbexSubsHelper
 
         return add_query_arg($query, home_url('/'));
     }
+
+    public function is_ready()
+    {
+        return (!empty($this->enabled) && !empty($this->api_key) && !empty($this->access_token) && $this->enabled === 'yes');
+    }
+
+    public function is_wcs_active()
+    {
+        return (!empty($this->integration) && $this->integration === 'wcs' && get_option('woocommerce_subscriptions_is_active'));
+    }
+
+    /**
+	 * Check if Order has a Mobbex Subscription product.
+	 *
+	 * @param integer $order_id
+	 */
+    public function has_subscription($order_id)
+    {
+        // Search subscription products in Order
+        $order = wc_get_order($order_id);
+
+		foreach ($order->get_items() as $item) {
+            $product_id = $item->get_product()->get_id();
+
+            if (Mbbx_Subs_Product::is_subscription($product_id))
+                return true;
+        }
+
+        return false;
+	}
+
+    /**
+	 * Checks if page is pay for order and change subs payment page.
+	 */
+    public static function is_subs_change_method()
+    {
+		return (isset($_GET['pay_for_order']) && isset($_GET['change_payment_method']));
+	}
+
+    /**
+	 * Get payment state from Mobbex status code.
+     * 
+     * @param int|string $status
+     * 
+     * @return string "approved" | "on-hold" | "cancelled"
+	 */
+    public static function get_state($status)
+    {
+        if ($status == 2 || $status == 3 || $status == 100 || $status == 201) {
+            return 'on-hold';
+        } else if ($status == 4 || $status >= 200 && $status < 400) {
+            return 'approved';
+        } else {
+            return 'cancelled';
+        }
+	}
+
+    /**
+     * Execute subscription charge manually using Mobbex API.
+     * 
+     * @param string $mbbx_subscription_uid
+     * @param string $mbbx_subscriber_uid
+     * @param integer $total
+     * @return array|null $response_result
+     */
+    public function execute_charge($mbbx_subscription_uid, $mbbx_subscriber_uid, $total)
+    {
+        $url = str_replace(['{id}', '{sid}'], [$mbbx_subscription_uid, $mbbx_subscriber_uid], MOBBEX_SUBSCRIPTION);
+        $body = [
+            'total' => $total,
+            'test' => $this->test_mode,
+        ];
+
+        $response = wp_remote_post($url, [
+            'headers' => [
+                'cache-control' => 'no-cache',
+                'content-type' => 'application/json',
+                'x-api-key' => $this->api_key,
+                'x-access-token' => $this->access_token,
+            ],
+
+            'body' => json_encode($body),
+            'data_format' => 'body',
+        ]);
+
+        if (!is_wp_error($response)) {
+            $response = json_decode($response['body'], true);
+
+            if (!empty($response['result']))
+                return $response['result'];
+        }
+
+        return;
+    }
+
+    /**
+     * Retry Subscription execution using Mobbex API.
+     * 
+     * @param string|int $order_id
+     * @param string|int $execution_id
+     * 
+     * @return bool $result
+     */
+    public function retry_execution($order_id, $execution_id)
+    {
+        if (!$this->is_ready()) {
+            throw new Exception(__('Plugin is not ready', 'mobbex-subs-for-woocommerce'));
+        }
+
+        // Query params
+        $params = [
+            'id'  => get_post_meta($order_id, 'mobbex_subscription_uid'),
+            'sid' => get_post_meta($order_id, 'mobbex_subscriber_uid'),
+            'eid' => $execution_id,
+        ];
+
+        // Retry execution
+        $response = wp_remote_get(str_replace(['{id}', '{sid}', '{eid}'], $params, MOBBEX_RETRY_EXECUTION), [
+            'headers' => [
+                'cache-control' => 'no-cache',
+                'content-type' => 'application/json',
+                'x-api-key' => $this->api_key,
+                'x-access-token' => $this->access_token,
+            ],
+        ]);
+
+        if (!is_wp_error($response)) {
+            $response = json_decode($response['body'], true);
+
+            if (!empty($response['result'])) {
+                return true;
+            }
+        }
+
+        throw new Exception(__('An error occurred in the execution', 'mobbex-subs-for-woocommerce'));
+    }
+
+    /*public function save_retried_execution($order_id, $execution_id)
+    {
+        $executions = get_post_meta($order_id, 'mbbxs_webhooks', true);
+
+        foreach ($executions as $key => $execution) {
+            if (isset($execution['data']['execution']['uid']) && $execution['data']['execution']['uid'] == $execution_id) {
+                $executions[$key]['']
+            }
+        }
+    }*/
 }
--- includes/utils.php
+++ includes/utils.php
@@ -4,12 +4,13 @@
 define('MOBBEX_SUBSCRIPTION', 'https://api.mobbex.com/p/subscriptions/{id}/subscriber/{sid}/execution');
 define('MOBBEX_CREATE_SUBSCRIPTION', 'https://api.mobbex.com/p/subscriptions');
 define('MOBBEX_CREATE_SUBSCRIBER', 'https://api.mobbex.com/p/subscriptions/{id}/subscriber');
+define('MOBBEX_RETRY_EXECUTION', 'https://api.mobbex.com/p/subscriptions/{id}/subscriber/{sid}/execution/{eid}/action/retry');
 
 // Coupon URL
 define('MOBBEX_SUBS_COUPON', 'https://mobbex.com/console/{entity.uid}/operations/?oid={payment.id}');
 
-define('MOBBEX_SUBS_WC_GATEWAY', 'WC_Gateway_Mobbex_Subs');
+define('MOBBEX_SUBS_WC_GATEWAY', 'WC_Gateway_Mbbx_Subs');
 define('MOBBEX_SUBS_WC_GATEWAY_ID', 'mobbex_subs');
 
-define('MOBBEX_SUBS_VERSION', '1.0.2');
+define('MOBBEX_SUBS_VERSION', '2.0.0');
 define('MOBBEX_SUBS_EMBED_VERSION', '1.0.17');
