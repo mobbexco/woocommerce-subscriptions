40e32d1df4b565db07c163f92164a3c9e2ce4d95
jlmobbex <javier.lopez@mobbex.com>
2022-01-25 13:02:51 -0300

    Update version and add builder script

---
+++ build.bat
@@ -0,0 +1,10 @@
+set ver="2.1.0"
+
+:: Create directory with plugin files
+robocopy . woocommerce-mobbex-subs /MIR /XD .git .vscode woocommerce-mobbex-subs /XF .gitignore build.bat readme.md *.zip ignorethis*
+
+:: Compress archive
+7z a -tzip wcs-mobbex.%ver%.zip woocommerce-mobbex-subs
+
+:: Delete directory
+rd /s /q woocommerce-mobbex-subs
