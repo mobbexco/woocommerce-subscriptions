cd3d8b8f87b48a63c05b5ec467c65494b663c5d6
jlmobbex <javier.lopez@mobbex.com>
2022-04-21 13:06:55 -0300

    Update version

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-set ver="2.1.0"
+set ver="2.1.1"
 
 :: Create directory with plugin files
 robocopy . woocommerce-mobbex-subs /MIR /XD .git .vscode woocommerce-mobbex-subs /XF .gitignore build.bat readme.md *.zip ignorethis*
