0c8dcdc9e994885540b1ca28b3a893b8f61dcbb0
mobbexLuis <luis.scarrone@mobbex.com>
2024-04-16 12:15:41 -0300

    updated config settings

--- src/admin/config-options.php
+++ src/admin/config-options.php
@@ -5,28 +5,29 @@ defined('ABSPATH') || exit;
 return [
 
     /** Subscription options */
-    'subscription_configuration_tab' => [
-        'title' => __('Mobbex Subscription Configuration', 'mobbex-subs-for-woocommerce'),
+    'subscription_tab' => [
+        'title' => __('Mobbex Subscription Configuration', 'mobbex-for-woocommerce'),
         'type'  => 'title',
         'class' => 'mbbx-tab mbbx-tab-subscription',
     ],
     
     'integration' => [
-        'title'       => __('Integrate with', 'mobbex-subs-for-woocommerce'),
+        'title'       => __('Integrate with', 'mobbex-for-woocommerce'),
         'class'       => 'mbbx-into-subscription',
         'type'        => 'select',
-        'description' => __('Integrate this plugin with other subscriptions plugins. Detected integrations are displayed', 'mobbex-subs-for-woocommerce'),
+        'description' => __('Integrate this plugin with other subscriptions plugins. Detected integrations are displayed', 'mobbex-for-woocommerce'),
         'desc_tip'    => true,
         'options'     => [
-            '' => __('None', 'mobbex-subs-for-woocommerce'),
+            ''      => __('None', 'mobbex-for-woocommerce'),
+            'wcs'   => __('Woocommerce Subscriptions', 'mobbex-for-woocommerce')
             ]
         ],
 
     'send_subscriber_email' => [
-        'title'   => __('Enable emails to Subscriber', 'mobbex-subs-for-woocommerce'),
+        'title'   => __('Enable emails to Subscriber', 'mobbex-for-woocommerce'),
         'class'   => 'mbbx-into-subscription',
         'type'    => 'checkbox',
-        'label'   => __('Enable Subscriber Emails', 'mobbex-subs-for-woocommerce'),
+        'label'   => __('Enable Subscriber Emails', 'mobbex-for-woocommerce'),
         'default' => 'yes'
     ]
 ];
