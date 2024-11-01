f9dcd9ae6ab526da793136823b80a6ce260b840b
mobbexLuis <luis.scarrone@mobbex.com>
2024-11-01 11:29:01 -0300

    removed obsolete difine

--- utils/definitions.php
+++ utils/definitions.php
@@ -8,12 +8,10 @@ define('MOBBEX_SUBSCRIPTION', 'https://api.mobbex.com/p/subscriptions/{id}/subsc
 define('MOBBEX_RETRY_EXECUTION', 'https://api.mobbex.com/p/subscriptions/{id}/subscriber/{sid}/execution/{eid}/action/retry');
 
 // Coupon URL
-define('MOBBEX_SUBS_DIRECTORY', plugin_dir_url(__DIR__));
+define('MOBBEX_SUBS_URL', plugin_dir_url(__DIR__));
 define('MOBBEX_SUBS_COUPON', 'https://mobbex.com/console/{entity.uid}/operations/?oid={payment.id}');
 
 define('MOBBEX_SUBS_WC_GATEWAY', 'WC_Gateway_Mbbx_Subs');
 define('MOBBEX_SUBS_WC_GATEWAY_ID', 'mobbex_subs');
 
 define('MOBBEX_SUBS_VERSION', '4.0.0');
-
-define('MOBBEX_SUBS_DIR', plugin_dir_path(__DIR__));
